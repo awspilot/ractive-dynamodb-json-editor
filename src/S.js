27ea777ba811f906ea52512f536eb98a2950b244
adrianpraja <adrian@databank.ro>
2019-10-21 15:13:57 +0300

    edit string in textarea

--- src/S.js
+++ src/S.js
@@ -17,13 +17,14 @@ export default Ractive.extend({
 							<td class='jsoneditor-tree'>\
 								<div contenteditable='true' spellcheck='false' class='jsoneditor-field'>{{key}} </div>\
 							</td>\
-							<td class='jsoneditor-datatype'>String</td>\
+							<!-- <td class='jsoneditor-datatype'>String</td> -->\
 							<td class='jsoneditor-separator'>:</td>\
-							<td class='jsoneditor-tree'>\
-								<input value='{{value}}' class='jsoneditor-input jsoneditor-string' />\
-								<!--\
-								<div contenteditable='true' spellcheck='false' class='jsoneditor-value jsoneditor-string' title=''>{{ value }}</div>\
-								-->\
+							<td class='jsoneditor-tree' style='width: 100%;'>\
+								{{#if editing}}\
+									<textarea class='jsoneditor-input jsoneditor-string' style='height: {{textarea_height}}px;line-height: {{line_height}}px' on-focus='focus' on-blur='blur' on-keyup='keyup' value='{{value}}'></textarea>\
+								{{else}}\
+									<div class='jsoneditor-value jsoneditor-string' style='cursor: pointer;height: {{line_height}}px;line-height: {{line_height}}px;overflow: hidden;' on-click='startEditing'>{{ JSON.stringify(value).substr(1).slice(0,-1) }}</div>\
+								{{/if}}\
 							</td>\
 						</tr>\
 					</tbody>\
@@ -31,4 +32,32 @@ export default Ractive.extend({
 			</td>\
 		</tr>\
 		",
+		data: function() {
+			return {
+				line_height: 22,
+				textarea_height: 22,
+			}
+		},
+		on: {
+			startEditing: function( el ) {
+				var node = el.node.parentNode;
+				this.set('editing',true)
+				if (node.getElementsByTagName('textarea').length)
+					node.getElementsByTagName('textarea')[0].focus()
+
+			},
+			focus: function() {
+
+				var lines = this.get('value').split("\n").length
+				this.set('textarea_height', lines * this.get('line_height') )
+			},
+			blur: function() {
+				this.set('editing', false )
+			},
+			keyup: function() {
+				var lines = this.get('value').split("\n").length
+				this.set('textarea_height', lines * this.get('line_height') )
+				console.log('keydown', lines )
+			},
+		}
 	})
