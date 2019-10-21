c8990770deb0aa4ad5fe51ed5d255578939c0e99
adrianpraja <adrian@databank.ro>
2019-10-21 12:12:39 +0300

    StringSet support

---
+++ src/SS.js
@@ -0,0 +1,53 @@
+
+import S from './S';
+
+
+export default Ractive.extend({
+	components: {
+		S: S,
+	},
+	template: "\
+\
+	<tr class=' jsoneditor-expandable'>\
+		<td>\
+			<button type='button' class='jsoneditor-button jsoneditor-dragarea'></button>\
+		</td>\
+		<td>\
+			<button type='button' class='jsoneditor-button jsoneditor-contextmenu'></button>\
+		</td>\
+		<td>\
+			<table class='jsoneditor-values' style='border-collapse: collapse; margin-left: {{ level * 24 }}px;'>\
+				<tbody>\
+					<tr>\
+						<td class='jsoneditor-tree'>\
+							<button type='button' class='jsoneditor-button {{#if open}}jsoneditor-expanded{{else}}jsoneditor-collapsed{{/if}}' on-click='@this.toggle('open')'></button>\
+						</td>\
+						<td class='jsoneditor-tree'>\
+							<div contenteditable='true' spellcheck='false' class='jsoneditor-field'>{{key}}</div>\
+						</td>\
+						<td class='jsoneditor-datatype'>List[{{ value.length }}]</td>\
+						<td class='jsoneditor-tree'></td>\
+						<td class='jsoneditor-tree'>\
+							<div class='jsoneditor-value jsoneditor-array'></div>\
+						</td>\
+					</tr>\
+				</tbody>\
+			</table>\
+		</td>\
+	</tr>\
+\
+	{{#if open}}\
+	{{#value}}\
+		<S key={{@index}} value={{ . }} level='{{ level + 1 }}' />\
+	{{/value}}\
+	{{/if}}\
+\
+	<tr class='jsoneditor-append'></tr>\
+\
+	",
+	data: function() {
+		return {
+			open: false,
+		}
+	},
+})
