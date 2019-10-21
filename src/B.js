ead0b21b7bf33ae54baa88014e988cc37b0d9d30
adrianpraja <adrian@databank.ro>
2019-10-21 09:52:03 +0300

    +

---
+++ src/B.js
@@ -0,0 +1,32 @@
+
+export default Ractive.extend({
+	template: "\
+	<tr class=''>\
+		<td>\
+			<button type='button' class='jsoneditor-button jsoneditor-dragarea' title=''></button>\
+		</td>\
+		<td>\
+			<button type='button' class='jsoneditor-button jsoneditor-contextmenu' title=''></button>\
+		</td>\
+		<td>\
+			<table class='jsoneditor-values' style='border-collapse: collapse; margin-left: {{ level * 24 }}px;'>\
+				<tbody>\
+					<tr>\
+						<td class='jsoneditor-tree'>\
+							<button type='button' class='jsoneditor-button jsoneditor-invisible' title=''></button>\
+						</td>\
+						<td class='jsoneditor-tree'>\
+							<div contenteditable='true' spellcheck='false' class='jsoneditor-field'>{{key}}</div>\
+						</td>\
+						<td class='jsoneditor-datatype'>Binary</td>\
+						<td class='jsoneditor-separator'>:</td>\
+						<td class='jsoneditor-tree'>\
+							<div contenteditable='true' spellcheck='false' class='jsoneditor-value jsoneditor-binary' title=''>{{ value }}</div>\
+						</td>\
+					</tr>\
+				</tbody>\
+			</table>\
+		</td>\
+	</tr>\
+	",
+})
