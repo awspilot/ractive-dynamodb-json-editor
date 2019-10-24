5c19de1c90791b4883ef61b7fe40bedcc2a0a285
adrianpraja <adrian@databank.ro>
2019-10-24 10:37:05 +0300

    update

--- src/S.js
+++ src/S.js
@@ -1,43 +1,43 @@
 export default Ractive.extend({
-		template: "\
-		<tr>\
-			<td>\
-				<button type='button' class='jsoneditor-button jsoneditor-dragarea' ></button>\
-			</td>\
-			<td>\
-				<button type='button' class='jsoneditor-button jsoneditor-contextmenu' ></button>\
-			</td>\
-			<td>\
-				<table class='jsoneditor-values' style='border-collapse: collapse; margin-left: {{ level * 24 }}px;'>\
-					<tbody>\
-						<tr>\
-							<td class='jsoneditor-tree'>\
-								<button type='button' class='jsoneditor-button jsoneditor-invisible' ></button>\
-							</td>\
-							<td class='jsoneditor-tree'>\
-								<div contenteditable='false' spellcheck='false' class='jsoneditor-field'>{{key}} </div>\
-							</td>\
-							<!-- <td class='jsoneditor-datatype'>String</td> -->\
-							<td class='jsoneditor-separator'>:</td>\
-							<td class='jsoneditor-tree' style='width: 100%;'>\
-								{{#if editing}}\
-									<textarea class='jsoneditor-input jsoneditor-string' style='height: {{textarea_height}}px;line-height: {{line_height}}px' on-focus='focus' on-blur='blur' on-keyup='keyup' value='{{value}}'></textarea>\
-								{{else}}\
-									<input class='jsoneditor-input jsoneditor-string' value='{{ JSON.stringify(value) }}' readonly='true' on-click='startEditing' /> \
-									<!--<div class='jsoneditor-value jsoneditor-string' style='cursor: pointer;height: {{line_height}}px;line-height: {{line_height}}px;overflow: hidden;' on-click='startEditing'>{{ JSON.stringify(value) }}</div>-->\
-								{{/if}}\
-							</td>\
-						</tr>\
-					</tbody>\
-				</table>\
-			</td>\
-			<td>\
-				<button class='jsoneditor-button btn-delete-attribute' on-click='delete' >\
-					<div class='trash-solid icon'></div>\
-				</button>\
-			</td>\
-		</tr>\
-		",
+		template: `
+		<tr>
+			<td>
+				<button type='button' class='jsoneditor-button jsoneditor-dragarea' ></button>
+			</td>
+			<td>
+				<button type='button' class='jsoneditor-button jsoneditor-contextmenu' ></button>
+			</td>
+			<td>
+				<table class='jsoneditor-values' style='border-collapse: collapse; margin-left: {{ level * 24 }}px;'>
+					<tbody>
+						<tr>
+							<td class='jsoneditor-tree'>
+								<button type='button' class='jsoneditor-button jsoneditor-invisible' ></button>
+							</td>
+							<td class='jsoneditor-tree'>
+								<div contenteditable='false' spellcheck='false' class='jsoneditor-field'>{{key}} </div>
+							</td>
+							<!-- <td class='jsoneditor-datatype'>String</td> -->
+							<td class='jsoneditor-separator'>:</td>
+							<td class='jsoneditor-tree' style='width: 100%;'>
+								{{#if editing}}
+									<textarea class='jsoneditor-input jsoneditor-string' style='height: {{textarea_height}}px;line-height: {{line_height}}px' on-focus='focus' on-blur='blur' on-keyup='keyup' value='{{value}}'></textarea>
+								{{else}}
+									<input class='jsoneditor-input jsoneditor-string' value='{{ JSON.stringify(value) }}' readonly='true' on-click='startEditing' />
+									<!--<div class='jsoneditor-value jsoneditor-string' style='cursor: pointer;height: {{line_height}}px;line-height: {{line_height}}px;overflow: hidden;' on-click='startEditing'>{{ JSON.stringify(value) }}</div>-->
+								{{/if}}
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</td>
+			<td>
+				<button class='jsoneditor-button btn-delete-attribute' on-click='delete' >
+					<div class='trash-solid icon'></div>
+				</button>
+			</td>
+		</tr>
+		`,
 		data: function() {
 			return {
 				line_height: 22,
