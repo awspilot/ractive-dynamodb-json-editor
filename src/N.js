61bab6dfe45cc0af81a2b43a820e45ff58ac8e53
adrianpraja <adrian@databank.ro>
2019-10-24 10:48:28 +0300

    update

--- src/N.js
+++ src/N.js
@@ -1,42 +1,42 @@
 
 export default Ractive.extend({
-	template: "\
-	<tr>\
-		<td>\
-			<button type='button' class='jsoneditor-button jsoneditor-dragarea' ></button>\
-		</td>\
-		<td>\
-			<button type='button' class='jsoneditor-button jsoneditor-contextmenu' ></button>\
-		</td>\
-		<td>\
-			<table class='jsoneditor-values' style='border-collapse: collapse; margin-left: {{ level * 24 }}px;'>\
-				<tbody>\
-					<tr>\
-						<td class='jsoneditor-tree'>\
-							<button type='button' class='jsoneditor-button jsoneditor-invisible' ></button>\
-						</td>\
-						<td class='jsoneditor-tree'>\
-							<div contenteditable='false' spellcheck='false' class='jsoneditor-field'>{{key}}</div>\
-						</td>\
-						<!--<td class='jsoneditor-datatype'>Number</td>-->\
-						<td class='jsoneditor-separator'>:</td>\
-						<td class='jsoneditor-tree' style='width: 100%;'>\
-							<input type='number' value='{{mirror_value}}' class='jsoneditor-input jsoneditor-number'  />\
-							<!--\
-							<div contenteditable='true' spellcheck='false' class='jsoneditor-value jsoneditor-number' >{{ value }}</div>\
-							-->\
-						</td>\
-					</tr>\
-				</tbody>\
-			</table>\
-		</td>\
-		<td>\
-			<button class='jsoneditor-button btn-delete-attribute' on-click='delete' >\
-				<div class='trash-solid icon'></div>\
-			</button>\
-		</td>\
-	</tr>\
-	",
+	template: `
+	<tr>
+		<td>
+			<button type='button' class='jsoneditor-button jsoneditor-dragarea' ></button>
+		</td>
+		<td>
+			<button type='button' class='jsoneditor-button jsoneditor-contextmenu' ></button>
+		</td>
+		<td>
+			<table class='jsoneditor-values' style='border-collapse: collapse; margin-left: {{ level * 24 }}px;'>
+				<tbody>
+					<tr>
+						<td class='jsoneditor-tree'>
+							<button type='button' class='jsoneditor-button jsoneditor-invisible' ></button>
+						</td>
+						<td class='jsoneditor-tree'>
+							<div contenteditable='false' spellcheck='false' class='jsoneditor-field'>{{key}}</div>
+						</td>
+						<!--<td class='jsoneditor-datatype'>Number</td>-->
+						<td class='jsoneditor-separator'>:</td>
+						<td class='jsoneditor-tree' style='width: 100%;'>
+							<input type='number' value='{{mirror_value}}' class='jsoneditor-input jsoneditor-number'  />
+							<!--
+							<div contenteditable='true' spellcheck='false' class='jsoneditor-value jsoneditor-number' >{{ value }}</div>
+							-->
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</td>
+		<td>
+			<button class='jsoneditor-button btn-delete-attribute' on-click='delete' >
+				<div class='trash-solid icon'></div>
+			</button>
+		</td>
+	</tr>
+	`,
 	data: function() {
 		return {
 			mirror_value: '',
