39c48962a88dd5006cf9e7771277e4cb1ba37855
adrianpraja <adrian@databank.ro>
2019-10-24 10:50:36 +0300

    update

--- src/NULL.js
+++ src/NULL.js
@@ -1,40 +1,40 @@
 
 export default Ractive.extend({
-	template: "\
-	<tr>\
-		<td>\
-			<button type='button' class='jsoneditor-button jsoneditor-dragarea' ></button>\
-		</td>\
-		<td>\
-			<button type='button' class='jsoneditor-button jsoneditor-contextmenu' ></button>\
-		</td>\
-		<td>\
-			<table class='jsoneditor-values' style='border-collapse: collapse; margin-left: {{ level * 24 }}px;'>\
-				<tbody>\
-					<tr>\
-						<td class='jsoneditor-tree'>\
-							<button type='button' class='jsoneditor-button jsoneditor-invisible' ></button>\
-						</td>\
-						<td class='jsoneditor-tree'>\
-							<div contenteditable='false' spellcheck='false' class='jsoneditor-field'>{{key}}</div>\
-						</td>\
-						<!--<td class='jsoneditor-datatype'>Null</td>-->\
-						<td class='jsoneditor-separator'>:</td>\
-						<td class='jsoneditor-tree'>\
-							<div class='jsoneditor-input jsoneditor-null' >null</div>\
-							<!--<div class='jsoneditor-value jsoneditor-null' >null</div>-->\
-						</td>\
-					</tr>\
-				</tbody>\
-			</table>\
-		</td>\
-		<td>\
-			<button class='jsoneditor-button btn-delete-attribute' on-click='delete' >\
-				<div class='trash-solid icon'></div>\
-			</button>\
-		</td>\
-	</tr>\
-	",
+	template: `
+	<tr>
+		<td>
+			<button type='button' class='jsoneditor-button jsoneditor-dragarea' ></button>
+		</td>
+		<td>
+			<button type='button' class='jsoneditor-button jsoneditor-contextmenu' ></button>
+		</td>
+		<td>
+			<table class='jsoneditor-values' style='border-collapse: collapse; margin-left: {{ level * 24 }}px;'>
+				<tbody>
+					<tr>
+						<td class='jsoneditor-tree'>
+							<button type='button' class='jsoneditor-button jsoneditor-invisible' ></button>
+						</td>
+						<td class='jsoneditor-tree'>
+							<div contenteditable='false' spellcheck='false' class='jsoneditor-field'>{{key}}</div>
+						</td>
+						<!--<td class='jsoneditor-datatype'>Null</td>-->
+						<td class='jsoneditor-separator'>:</td>
+						<td class='jsoneditor-tree'>
+							<div class='jsoneditor-input jsoneditor-null' >null</div>
+							<!--<div class='jsoneditor-value jsoneditor-null' >null</div>-->
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</td>
+		<td>
+			<button class='jsoneditor-button btn-delete-attribute' on-click='delete' >
+				<div class='trash-solid icon'></div>
+			</button>
+		</td>
+	</tr>
+	`,
 	on: {
 		delete: function() {
 			this.parent.delete_key( this.get('key') )
